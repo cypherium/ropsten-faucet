6f74111bd2079413669731d0ac99da87223b3867
japson1987 <276286171@qq.com>
2022-02-28 16:52:28 +0800

    handleCaptchaResponse

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.notificationSystem = null;
 
     this.addNotification = this.addNotification.bind(this);
-   // this.handleCaptchaResponse = this.handleCaptchaResponse.bind(this);
+    this.handleCaptchaResponse = this.handleCaptchaResponse.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
